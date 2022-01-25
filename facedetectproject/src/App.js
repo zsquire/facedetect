500d0427e476cf172607cf8217579dd3fbf8773d
zsquire <zhfprodev@gmail.com>
2022-01-24 22:59:59 -0800

    file structure setup, basic landing page with miniimal styles using react tilt and cra

---
+++ facedetectproject/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation"
+import Logo from "./components/Logo/Logo"
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Logo />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+
+    )
+  }
+}
+
+export default App;
